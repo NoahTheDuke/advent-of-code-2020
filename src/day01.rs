46d7209ef5933ef4328de757eccece71a0d4c796
Noah Bogart <nbogart@batterii.com>
2020-12-01 23:55:28 -0500

    Day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,32 @@
+use itertools::Itertools;
+
+// --- Day 1: Report Repair ---
+
+fn search(numbers: Vec<usize>, k: usize) -> Option<usize> {
+    for n in numbers.into_iter().combinations(k) {
+        if n.iter().sum::<usize>() == 2020 {
+            return Some(n.iter().product());
+        }
+    }
+    return None;
+}
 
 pub fn part1(inp: String) {
-    println!("{}", inp.chars().rev().collect::<String>());
+    let numbers = inp.lines().map(|x| x.parse().unwrap()).collect::<Vec<usize>>();
+    match search(numbers, 2) {
+        Some(result) => {
+            println!("{:?}", result);
+        }
+        None => {}
+    }
+}
+
+pub fn part2(inp: String) {
+    let numbers = inp.lines().map(|x| x.parse().unwrap()).collect::<Vec<usize>>();
+    match search(numbers, 3) {
+        Some(result) => {
+            println!("{:?}", result);
+        }
+        None => {}
+    }
 }

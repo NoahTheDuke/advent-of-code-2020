07b762dbaf8c710387676377b8d12e20603cf3c7
Noah Bogart <nbogart@batterii.com>
2020-12-09 15:10:15 -0500

    An even faster day 9 rust solution

--- rust/src/day09.rs
+++ rust/src/day09.rs
@@ -9,8 +9,8 @@ pub fn part1(input: String) -> usize {
 
     while let Some(next) = lines.pop_front() {
         let mut found = false;
-        'outer: for i in 0..25 {
-            for j in i..25 {
+        'outer: for i in 0..24 {
+            for j in i + 1..25 {
                 match (preamble.get(i), preamble.get(j)) {
                     (Some(f), Some(s)) => {
                         if f + s == next {
@@ -37,18 +37,47 @@ static GOAL: u64 = 50047984;
 
 pub fn part2(input: String) -> usize {
     let lines: Vec<_> = input.lines().map(|l| l.parse::<u64>().unwrap()).collect();
-    let mut val = 0;
+    let mut result = 0;
 
-    'outer: for i in 1..lines.len() {
-        for j in (i + 1)..lines.len() {
-            let test = &lines[i..j];
-            let result: u64 = test.iter().sum();
-            if result == GOAL {
-                val = test.iter().min().unwrap() + test.iter().max().unwrap();
-                break 'outer;
-            }
+    // // 19ms 254µs
+    // for i in 1..lines.len() {
+    //     for j in (i + 1)..lines.len() {
+    //         let test = &lines[i..j];
+    //         let sum: u64 = test.iter().sum();
+    //         if sum == GOAL {
+    //             result = test.iter().min().unwrap() + test.iter().max().unwrap();
+    //         }
+    //     }
+    // }
+
+    // // 76µs
+    // 'outer: for i in 2..lines.len() {
+    //     for window in lines.windows(i) {
+    //         let sum: u64 = window.iter().sum();
+    //         if sum == GOAL {
+    //             result = window.iter().min().unwrap() + window.iter().max().unwrap();
+    //             break 'outer;
+    //         }
+    //     }
+    // }
+
+    // 1µs
+    let mut tail = 0;
+    let mut head = 1;
+    let mut sum = lines[0];
+    while head < lines.len() {
+        if sum < GOAL {
+            sum += lines[head];
+            head += 1;
+        } else if sum > GOAL {
+            sum -= lines[tail];
+            tail += 1;
+        } else {
+            let set = &lines[tail..head + 1];
+            result = set.iter().min().unwrap() + set.iter().max().unwrap();
+            break;
         }
     }
 
-    val as usize
+    result as usize
 }
